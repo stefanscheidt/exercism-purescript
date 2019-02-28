fc0965179dfe9e690eabe2bad9c29dd2f1ac5647
Stefan Scheidt <stefan.scheidt@icloud.com>
2019-02-28 15:58:43 +0100

    initial atbash-cipher commit.

--- atbash-cipher/src/AtbashCipher.purs
+++ atbash-cipher/src/AtbashCipher.purs
@@ -2,3 +2,11 @@ module AtbashCipher
   ( decode
   , encode
   ) where
+
+import Data.Maybe (Maybe(..))
+
+encode :: String -> Maybe String
+encode = Just
+
+decode :: String -> Maybe String
+decode = Just
