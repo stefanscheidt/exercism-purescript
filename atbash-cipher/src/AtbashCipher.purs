b427d88077e8a136be8438feadcc6fef5b0dc986
Stefan Scheidt <stefan.scheidt@icloud.com>
2019-03-08 21:12:17 +0100

    decode text plus refactorings.

--- atbash-cipher/src/AtbashCipher.purs
+++ atbash-cipher/src/AtbashCipher.purs
@@ -8,7 +8,7 @@ import Prelude
 import Data.Array (drop, filter, reverse, snoc, take, zip)
 import Data.Char.Unicode (isAlphaNum, toLower)
 import Data.Enum (enumFromTo)
-import Data.Map (fromFoldable, lookup)
+import Data.Map (Map, fromFoldable, lookup)
 import Data.Maybe (Maybe(..), fromMaybe)
 import Data.String.CodeUnits (fromCharArray, toCharArray)
 import Data.String (joinWith)
@@ -16,26 +16,32 @@ import Data.Tuple (Tuple(..), fst)
 
 -- encode
 
+encodeChar :: Char -> Char
+encodeChar c = fromMaybe c $ lookup c cipher
+
 encode :: String -> Maybe String
 encode = Just <<< joinWith " " <<< map fromCharArray <<< package 5 <<< map encodeChar <<< normalize
+
+-- decode
+
+decodeChar :: Char -> Char
+decodeChar = encodeChar
+
+decode :: String -> Maybe String
+decode = Just <<< fromCharArray <<< map decodeChar <<< normalize
+
+-- helper
+
+cipher :: Map Char Char
+cipher = fromFoldable $ zip aToZ (reverse aToZ)
+  where 
+    aToZ = enumFromTo 'a' 'z'
 
 normalize :: String -> Array Char
 normalize = map toLower <<< filter isAlphaNum <<< toCharArray
 
-encodeChar :: Char -> Char
-encodeChar c = fromMaybe c $ lookup c encodeMap
-  where
-    aToZ = enumFromTo 'a' 'z'
-    encodeMap = fromFoldable $ zip aToZ (reverse aToZ)
-
 package :: forall a. Int -> Array a -> Array (Array a)
 package n as = fst $ package' (Tuple [] as)
   where
-    package' :: Tuple (Array (Array a)) (Array a) -> Tuple (Array (Array a)) (Array a)
     package' (Tuple bbs []) = Tuple bbs []
     package' (Tuple bbs bs) = package' (Tuple (snoc bbs (take n bs)) (drop n bs))
-
--- decode
-
-decode :: String -> Maybe String
-decode = Just
