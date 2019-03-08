7e52057278cdbcaa2c6095d497105f213d1e8e9d
Stefan Scheidt <stefan.scheidt@icloud.com>
2019-03-08 21:01:16 +0100

    encode text

--- atbash-cipher/src/AtbashCipher.purs
+++ atbash-cipher/src/AtbashCipher.purs
@@ -3,10 +3,39 @@ module AtbashCipher
   , encode
   ) where
 
-import Data.Maybe (Maybe(..))
+import Prelude
+
+import Data.Array (drop, filter, reverse, snoc, take, zip)
+import Data.Char.Unicode (isAlphaNum, toLower)
+import Data.Enum (enumFromTo)
+import Data.Map (fromFoldable, lookup)
+import Data.Maybe (Maybe(..), fromMaybe)
+import Data.String.CodeUnits (fromCharArray, toCharArray)
+import Data.String (joinWith)
+import Data.Tuple (Tuple(..), fst)
+
+-- encode
 
 encode :: String -> Maybe String
-encode = Just
+encode = Just <<< joinWith " " <<< map fromCharArray <<< package 5 <<< map encodeChar <<< normalize
+
+normalize :: String -> Array Char
+normalize = map toLower <<< filter isAlphaNum <<< toCharArray
+
+encodeChar :: Char -> Char
+encodeChar c = fromMaybe c $ lookup c encodeMap
+  where
+    aToZ = enumFromTo 'a' 'z'
+    encodeMap = fromFoldable $ zip aToZ (reverse aToZ)
+
+package :: forall a. Int -> Array a -> Array (Array a)
+package n as = fst $ package' (Tuple [] as)
+  where
+    package' :: Tuple (Array (Array a)) (Array a) -> Tuple (Array (Array a)) (Array a)
+    package' (Tuple bbs []) = Tuple bbs []
+    package' (Tuple bbs bs) = package' (Tuple (snoc bbs (take n bs)) (drop n bs))
+
+-- decode
 
 decode :: String -> Maybe String
 decode = Just
