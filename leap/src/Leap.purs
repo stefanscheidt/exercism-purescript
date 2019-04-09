1497dbd5efe75b5fbc529451ef6fe7afade90b00
Stefan Scheidt <stefan.scheidt@icloud.com>
2019-04-09 22:24:04 +0200

    Refactoring

--- leap/src/Leap.purs
+++ leap/src/Leap.purs
@@ -3,10 +3,14 @@ module Leap where
 import Prelude
 
 import Data.Int (rem)
+import Type.Data.Boolean (kind Boolean)
 
 isLeapYear :: Int -> Boolean
 isLeapYear year
-  | rem year 400 == 0 = true
-  | rem year 100 == 0 = false
-  | rem year   4 == 0 = true
-  | otherwise         = false
+  | year `divisibleBy` 400 = true
+  | year `divisibleBy` 100 = false
+  | year `divisibleBy`   4 = true
+  | otherwise              = false
+
+divisibleBy :: Int -> Int -> Boolean
+divisibleBy candidate divisor = rem candidate divisor == 0
